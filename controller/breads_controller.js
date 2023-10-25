bb3794366332782e2741840f31aa3647408eaf34
tamccullough88 <tamccullough88@gmail.com>
2023-10-25 19:23:10 -0400

    sending index and show data

--- controller/breads_controller.js
+++ controller/breads_controller.js
@@ -9,14 +9,18 @@ breads.get('/', (req,res) => {
     })
 })
 
-breads.get('/:index', (req,res) => {
-    const index = req.params.index
-    if (index > Bread.length - 1) {
-        res.send("No Such Bread")
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+    })
+    } else {
+    res.send('404')
     }
-    const bread = Bread[index]
-    res.send(bread)
 })
 
 
+
 module.exports = breads
+
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 // ROUTES
@@ -19,6 +20,12 @@ app.get('/', (req, res) => {
 
 const breadsController = require('./controller/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
 // LISTEN
 app.listen(PORT, () => {
